f5d3d2f3a8c721d92c2e994c81c02256b1ef2754
Petit <alois.petit@gmail.com>
2019-01-29 11:17:25 +0100

    Create new repo and dir structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <!-- Bibliothèques -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+    <!-- Meta tag -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Linkmmi - Le réseau social des MMI</title>
+
+    <!-- Links -->
+    <link rel="stylesheet" href="css/style.min.css">
+  </head>
+
+  <body>
+
+    <script src="js/script.js" type="text/javascript"></script>
+  </body>
+</html>
