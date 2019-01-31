908312c9bf39278cced3df5ae2905370283501ef
Jérémy Monatte <jeremy.monatte@gmail.com>
2019-01-31 12:33:13 +0100

    avancé page profil

--- profil.php
+++ profil.php
@@ -27,8 +27,39 @@
     <div class="content">
         <img src="https://picsum.photos/300/200/?random" alt="" class="banniere">
         <div class="profil">
-            <div class="descProfil">
-                bla bla bjfzcisdbjrvndicb
+            <img src="https://picsum.photos/300/300" class="profilPict">
+            <div class="contentProfil">
+                <div class="descProfil">
+                    <h3>Marie Durant<br><em>@Marie_Drt</em></h3>
+                    A étudier à <em>l'IUT du Puy en Velay</em><br>
+                    A étudier à <em>l'IUT du Puy en Velay</em><br>
+                    A étudier à <em>l'IUT du Puy en Velay</em><br>
+                    <div class="profilNum">Relations<br><em>12</em></div>
+                    <div class="profilNum">Publications<br><em>6</em></div>
+                </div>
+                <div class="subBtn">Éditer le profil</div>
+            </div>
+        </div>
+        <div class="flux">
+            <div class="article">
+                <div class="author">
+                    <img src="https://picsum.photos/300/300" alt="" class="authorPict">
+                    <h3>Marie Durant<br><em>@Marie_Drt</em></h3>
+                </div>
+                <div class="contentTxt">
+                    Cum saepe multa, tum memini domi in hemicyclio sedentem, ut solebat, cum et ego essem una et pauci admodum familiares, in eum sermonem illum incidere qui tum forte multis erat in ore. Meministi enim profecto, Attice, et eo magis, quod P. Sulpicio utebare multum, cum is tribunus plebis capitali odio a Q. Pompeio, qui tum erat consul, dissideret, quocum coniunctissime et amantissime vixerat, quanta esset hominum vel admiratio vel querella.
+                </div>
+                <div class="contentPict">
+                    <img src="https://picsum.photos/300/200/?random" alt="">
+                </div>
+                <div class="contentDetails">
+                    Publié le<em> 12h00 le 21 janvier 2019</em>
+                </div>
+            <div class="contentcomm">
+                <ul>
+                    <li><b>@Sarah_croche</b> Wouah ! Cette photo est magnifique.</li>
+                    <li><b>@Lara_clette</b> <em>@Vincent_tim</em> C'est cool</li>
+                </ul>
             </div>
         </div>
     </div>
