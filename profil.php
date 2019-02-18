2f3c92ff101382127cc60668cf5d1271e2ea5f16
Aparico Pablo <pablo.aprc@gmail.com>
2019-02-18 14:18:00 +0100

    Creation de la partie message

--- profil.php
+++ profil.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Profil</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/profil.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/profil.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="icofont/icofont.min.css">
 </head>
 <body>
