cb318ae98977d952d6afc73fcca55f1c9dab63da
Petit <alois.petit@gmail.com>
2019-02-18 18:05:43 +0100

    intégration de la connexion, inscription en cours

--- js/script.js
+++ js/script.js
@@ -1,4 +1,44 @@
 $(document).ready(function()
 {
-  
+
+
+  /*
+  * Connexion
+  *   - récupère les données saisies et compare avec les données issues de la bdd
+  *
+  * Données utilisées
+  */
+
+  // $("#connexion-submit").click(function()
+  // {
+  //   var login = $("#connexion-login").val();
+  //   var mdp = $("#connexion-password").val();
+  //
+  //   $.ajax
+  //   ({
+  //      url: "php/connexion.php",
+  //      type: "post",
+  //      data: { mailUser: login, passwordUser: mdp },
+  //      success: function (response)
+  //      {
+  //        switch(reponse)
+  //        {
+  //          case "connecte":
+  //
+  //          break;
+  //
+  //          case "mdp":
+  //          break;
+  //
+  //          case "email":
+  //          break;
+  //        }
+  //      },
+  //      error: function(jqXHR, textStatus, errorThrown) {
+  //         console.log(textStatus, errorThrow);
+  //      }
+  //   });
+  // });
+
+
 });
--- php/inscription.php
+++ php/inscription.php
@@ -14,12 +14,13 @@
 
   if(isset($_POST["submit"]) && !empty($_POST["mailUser"]) && !empty($_POST["passwordUser"]) && !empty($_POST["idUser"]) && !empty($_POST["nomUser"]) && !empty($_POST["prenomUser"]))
   {
-    $requete = $bdd->prepare("INSERT INTO utilisateur (idUser, nomUser, prenomUser, mailUser, passwordUser) VALUES (:id, :nom, :prenom, :mail, :mdp)");
+    $requete = $bdd->prepare("INSERT INTO utilisateur (idUser, nomUser, prenomUser, mailUser, passwordUser, atnameUser) VALUES (:id, :nom, :prenom, :mail, :mdp, :atname)");
     $requete->bindParam(':id', $_POST["idUser"]);
     $requete->bindParam(':nom', $_POST["nomUser"]);
     $requete->bindParam(':prenom', $_POST["prenomUser"]);
     $requete->bindParam(':mail', $_POST["mailUser"]);
     $requete->bindParam(':mdp', $_POST["passwordUser"]);
+    $requete->bindParam(':atname', $_POST["prenomUser"].".".$_POST["nomUser"]."#".$_POST["idUSer"]);
     $requete->execute();
   }
 
