d170370cf30b48e98179ae56430caa4dff02c63b
Maxence Deschamps <zemoax@hotmail.fr>
2019-03-28 20:05:06 +0100

    correction nav index

--- index.php
+++ index.php
@@ -98,34 +98,6 @@ session_start();
 <body>
   <?php if($connected) { include('php/nav.php'); echo '<div style="height: 60px;"></div>';  } ?>
 
-
-  <!--- Menu -->
-  <?php if($_SESSION["idUser"] != -1): ?>
-      <nav>
-          <div class="blcNav">
-              <figure id="data-user" data-number="<?php echo $current_user[0]->idUser; ?>">
-                <?php
-                  if(!empty($current_user[0]->photoUser && $current_user[0]->photoUser != 'NULL')) { ?> <img src="<?php echo $current_user[0]->photoUser; ?>" alt="Compte de <?php echo $current_user[0]->prenomUser.' '.$current_user[0]->nomUser; ?>"> <?php }
-                  else { ?> <img src="img-placeholder/bestLoutre.jpg" title="Compte de <?php echo $current_user[0]->prenomUser.' '.$current_user[0]->nomUser; ?>"> <?php }
-                ?>
-                <figcaption>
-                  <a href="profil.php"></a>
-                </figcaption>
-              </figure>
-              <form action="" method="post">
-                  <input type="text" placeholder="Rechercher">
-              </form>
-          </div>
-              <button class="btn-Icone active"><i class="icofont-link"></i></button>
-          <div class="blcNav">
-              <button class="btn-Icone active"><i class="icofont-home"></i></button>
-              <button class="btn-Icone"><i class="icofont-wechat"></i></button>
-              <button class="btn-Icone"><i class="icofont-heart-alt"></i></button>
-          </div>
-      </nav>
-    <?php endif; ?>
-    <!-- Fin menu -->
-
   <div class="section-connexion">
 
     <div class="section-left">
